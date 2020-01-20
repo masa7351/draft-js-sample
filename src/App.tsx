991bebe937798648cbe94beb179f9d773f59d837
Masanao Imai <masa7351@gmail.com>
2020-01-20 20:24:58 +0900

    Setup draft-js Remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Editor, EditorState } from 'draft-js';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [state, setState] = useState({
+    editorState: EditorState.createEmpty()
+  });
+
+  const changeHandler = (editorState: EditorState) => {
+    setState({ editorState });
+  };
+
+  return <Editor editorState={state.editorState} onChange={changeHandler} />;
+};
 
 export default App;
