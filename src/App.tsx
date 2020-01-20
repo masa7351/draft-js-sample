79323c3cd1ed4d75fd044ad91af76b845ec7a258
Masanao Imai <masa7351@gmail.com>
2020-01-20 22:21:55 +0900

    Add Sample that key command shortcut

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { Editor, EditorState } from 'draft-js';
+import React, { Fragment, useState } from 'react';
+import { Editor, EditorState, RichUtils, DraftHandleValue } from 'draft-js';
+import styled from 'styled-components';
 
 const App: React.FC = () => {
   const [state, setState] = useState({
@@ -10,7 +11,53 @@ const App: React.FC = () => {
     setState({ editorState });
   };
 
-  return <Editor editorState={state.editorState} onChange={changeHandler} />;
+  //  Key Command Shortcut
+  // https://draftjs.org/docs/api-reference-editor/
+  // Command + B = Bold
+  // Command + I = Italic
+  const keyCommandHandler = (
+    command: string,
+    editorState: EditorState,
+    eventTimeStamp: number
+  ): DraftHandleValue => {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      changeHandler(newState);
+      return 'handled';
+    } else {
+      return 'not-handled';
+    }
+  };
+
+  const boldHandler = () => {
+    const newState = RichUtils.toggleInlineStyle(state.editorState, 'BOLD');
+    setState({ editorState: newState });
+  };
+
+  return (
+    <Fragment>
+      <StyledBoldLabel>
+        範囲選択してタップするとBoldになります。
+        <button onClick={boldHandler}>Bold</button>
+      </StyledBoldLabel>
+      <StyledDiv>Command + B → Bold　／ Command + I → Italic</StyledDiv>
+      <StyledDiv>↓テキストを入力してください。</StyledDiv>
+      <Editor
+        editorState={state.editorState}
+        handleKeyCommand={keyCommandHandler}
+        onChange={changeHandler}
+      />
+    </Fragment>
+  );
 };
 
 export default App;
+
+const StyledBoldLabel = styled.div`
+  display: inline;
+  margin: 10px;
+`;
+
+const StyledDiv = styled.div`
+  margin: 10px;
+`;
